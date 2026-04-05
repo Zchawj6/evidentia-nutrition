b87705d9f84968c2e3e29b6d1c813f876a18e5a8
Zchawj6 <80202066+Zchawj6@users.noreply.github.com>
2026-04-05 23:57:07 +0100

    fix depth error

--- components/DepthToggle.jsx
+++ components/DepthToggle.jsx
@@ -61,6 +61,7 @@ export default function DepthToggle({ entry, summaryContent, indepthContent }) {
                 </tbody>
               </table>
             </div>
+          </div>
           )}
           {indepthContent}
         </div>
